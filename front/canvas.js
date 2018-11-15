ee6c4022cf33cb399a46ffeb2e73d24980f2e677
unknown <jose.wollinger@gmail.com>
2018-11-15 19:30:07 -0200

    canvas character with sprite

--- front/canvas.js
+++ front/canvas.js
@@ -1,16 +1,35 @@
 'use strict'
 
-var canvas, context;
 
-
-
-
-
-function initCanvas(){
-    canvas = document.getElementById('myCanvas')
-    context = canvas.getContext('2d')
-    context.fillStyle = '#FF0000'
-    context.fillRect(0, 0, 800, 500)
+var canvas, context
+var spriteCharacter = new Image()//document.getElementById('imgCharacter')
+spriteCharacter.src = 'Green-Cap-Character-16x18.png'
+spriteCharacter.onload = function(){
+    initCanvas()
 }
 
-initCanvas()
+function initCanvas() {
+    canvas = document.getElementById('myCanvas')
+    context = canvas.getContext('2d')
+    context.font = '14px Verdana'
+    mainLoop()
+}
+
+function mainLoop() {
+    let x = 0
+    let y = 0
+    setInterval(() =>{
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        drawCurrentPosition(x++,y++)
+    }, 500)
+
+}
+
+
+
+function drawCurrentPosition(posicaoX, posicaoY){
+    //context.drawImage(imagem, posicaoStripeX, posicaoSpriteY, widthImagem, heightImagem, posicaoXCanvas, posicaoYCanvas, widthImagem, heightImagem)
+    context.drawImage(spriteCharacter, 0, 0, 16, 18, posicaoX, posicaoY, 16, 18)
+    context.save()
+}
+
